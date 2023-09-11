a9c582de439dfaaaecfa0fb4c356916841c29659
Kántor Krisztián <kantor.krisztian@AD.LOCAL>
2023-09-11 13:03:16 +0200

    próba

--- Tóth Bence/09.05/09.05/java.js
+++
@@ -1,8 +0,0 @@
-function vesz(){
-	let nev = document.getElementById('nev').value;
-	let db = document.getElementById('kacsa').value;
-	let kacsaar = 5000;
-	let ar = db * kacsaar;
-
-	document.getElementById('eredmeny').innerHTML = nev + " most vesz " + db + " darab kacsát " + ar + " forintért. ";
-}
