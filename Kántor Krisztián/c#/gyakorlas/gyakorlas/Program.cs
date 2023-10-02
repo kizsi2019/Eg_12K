d76c25002826b62aaf7868c7ceda18e153897ded
Kántor Krisztián <kantor.krisztian@AD.LOCAL>
2023-10-02 13:13:18 +0200

    tombok

--- Kántor Krisztián/c#/gyakorlas/gyakorlas/Program.cs
+++ Kántor Krisztián/c#/gyakorlas/gyakorlas/Program.cs
@@ -158,7 +158,7 @@ namespace gyakorlas
             catch (FormatException ex)
             {
                 Console.WriteLine("Valami hiba történt: {0}", ex.Message);
-            }*/
+            }
 
             Console.WriteLine("Kettővel szorzó v. 1.0");
             Console.WriteLine("Adjon meg egy egész páros számot!");
@@ -183,6 +183,123 @@ namespace gyakorlas
             {
                 Console.WriteLine("Program vége. Nyomjon egy gombot a kilépéshez");
             }
+            
+
+            Console.WriteLine("Négyzet oldala");
+            try
+            {
+                int oldal =Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(4*oldal);
+                Console.WriteLine(oldal*oldal);
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("valami hiba: {0}", ex.Message);
+            }
+
+            Console.WriteLine("goto példa Kilépés: CTRL+C");
+            eleje:
+                Console.WriteLine("add meg a nved");
+                var nev = Console.ReadLine();
+                Console.WriteLine($"szia{nev}!");
+                goto eleje;
+
+            for (int i = 8; i < 100; i+=8)
+            {
+                Console.WriteLine(i);
+            }
+
+
+            int i = 10;
+            while (i-- > 0)
+            {
+                Console.Write(i);
+            }
+
+            int szam = -1;
+            do
+            {
+                try
+                {
+                    Console.WriteLine("Adjon meg egy 1 és 10 közötti páros számot!");
+                    string szoveg = Console.ReadLine();
+                    szam = Convert.ToInt32(szoveg);
+                }
+                catch (Exception)
+                {
+                    szam = -1;
+                }
+            }
+            while ((szam < 1) || (szam > 10) || ((szam % 2) != 0));
+
+            Console.WriteLine("A megadott szám: {0}", szam);
+
+            Console.WriteLine("Break példa");
+            for (int i = 0; i < 100; i++)
+            {
+                Console.Write("{0} ", i);
+                if (i == 10)
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine("Continue példa");
+            int j = 30;
+            while (j-- > 0)
+            {
+                if (j % 3 == 0)
+                {
+                    continue;
+                }
+                Console.Write("{0} ", j);
+            }
+
+            var gyumolcsok = new string[]
+            {
+                "alma", "körte", "szilva"
+            };
+
+            var bevitelek = new string[3];
+
+            for (int i = 0; i < bevitelek.Length; i++)
+            {
+                Console.WriteLine("{0}. bevitel: ", i);
+                bevitelek[i] = Console.ReadLine();
+            }
+
+            foreach (var gyumolcs in gyumolcsok)
+            {
+                Console.WriteLine(gyumolcs);
+            }
+            foreach (var bevitel in bevitelek)
+            {
+                Console.WriteLine(bevitel);
+            }
+
+            var tomb2d = new int[,]
+            {
+                { 1, 2, 3 },
+                { 4, 5, 6 },
+                { 7, 8, 9 }
+            };
+
+            Console.WriteLine("Bejárás for ciklusokkal");
+            //A length tulajdonság itt nem használható
+            //mivel a tömb 2d kiterjedésű. 
+            for (int i = 0; i < tomb2d.GetLength(0); i++)
+            {
+                for (int j = 0; j < tomb2d.GetLength(1); j++)
+                {
+                    Console.Write("{0} ", tomb2d[i, j]);
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("Bejárás foreach ciklusokkal");
+            foreach (var elem in tomb2d)
+            {
+                Console.Write("{0} ", elem);
+            }*/
 
             Console.ReadKey();
         }
