dac90f345ea648449f4e6b4feb5112d51a91fdea
Palásty Zsolt <palasty.zsolt19k@egressy.info>
2023-10-10 10:01:26 +0200

    A Beke tanárnő hova lett?

---
+++ Palásty Zsolt/Sudoku/KimenetiValtozo/KimenetiValtozo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace KimenetiValtozo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool paros;
+            Paros(10, out paros);
+            Console.ReadKey();
+        }
+        static void Paros(int a, out bool log)
+        {
+            if (a % 2 == 0)
+            {
+                log = true;
+                Console.WriteLine("Páros");
+            }
+            else
+            {
+                log = false;
+            }
+        }
+    }
+}
