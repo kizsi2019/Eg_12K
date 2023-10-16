ece170d52293afc6d4c5d18b7cab19f6c1b9d9e9
Palásty Zsolt <palasty.zsolt19k@egressy.info>
2023-10-16 13:56:18 +0200

    Hiányzik a Beke

---
+++ Palásty Zsolt/C#/LancoltLista/LancoltLista/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PeldaLancolt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lista = new LinkedList<string>();
+            //jobbról bővítés
+            lista.AddLast("egy");
+            lista.AddLast("példa");
+            lista.AddLast("program");
+            lista.AddLast("lenne");
+            //balról bővítés
+            lista.AddFirst("Ez");
+
+            Console.WriteLine("Lánc elemek száma: {0}", lista.Count);
+            Console.WriteLine("Lánc tartalma előre: ");
+            //a foreach automatikusan a value elemet szedi ki
+            foreach (var elem in lista)
+            {
+                Console.Write("{0} ", elem);
+            }
+
+            Console.WriteLine("\nLánc tartalma vissza: ");
+            var vissza = lista.Last;
+            while (vissza != null)
+            {
+                Console.Write("{0} ", vissza.Value);
+                vissza = vissza.Previous;
+            }
+            Console.ReadKey();
+        }
+    }
+}
