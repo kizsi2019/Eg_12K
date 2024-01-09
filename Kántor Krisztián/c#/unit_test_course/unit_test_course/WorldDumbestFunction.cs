ff248e7cfaa550038ef4900f5650c712cad72c07
Kántor Krisztián <Kantor.Krisztian@AD.LOCAL>
2024-01-09 14:35:21 +0100

    ads

---
+++ Kántor Krisztián/c#/unit_test_course/unit_test_course/WorldDumbestFunction.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace unit_test_course
+{
+    internal class WorldDumbestFunction
+    {
+        public string ReturnsPikachuIfZero(int num)
+        {
+            if (num == 0) return "Pikachu";
+            else return "Sajnos nem Pikachu";
+        }
+    }
+}
