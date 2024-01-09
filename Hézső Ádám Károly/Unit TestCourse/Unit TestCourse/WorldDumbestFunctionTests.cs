13506f48079860bde0a84a9f93e4ba26091b0b7e
Hézső Károly Ádám <hezso.adm19k@egressy.info>
2024-01-09 14:35:35 +0100

    orai

---
+++ Hézső Ádám Károly/Unit TestCourse/Unit TestCourse/WorldDumbestFunctionTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unit_TestCourse
+{
+    public class WorldDumbestFunctionTests
+    {
+        // Naming Convention ClassName_MethodName_ExpectedResults
+        public static void WorldDumbestFunction_ReturnPikacuIfZero_ReturnString()
+        {
+            try
+            {
+                //Arrange - Intézkedés - szerezd e a változókat osztályokat függvényeket
+                int num = 1;
+                WorldsDubestFunction worldDumbest = new WorldsDubestFunction();
+                // Act - cselekvés - futtasd a függvényt
+                string result = worldDumbest.ReturnsPikachuIfZero(num);
+
+                //Assert - érvényesítés - bármit ad vissza úgy legyen ahogy akarom
+                if (result == "PIKACHU!!!!")
+                {
+                    Console.WriteLine("Passed: WorldDumbestFunction_ReturnPikacuIfZero_ReturnString");
+                }
+                else
+                {
+                    Console.WriteLine("Failed: WorldDumbestFunction_ReturnPikacuIfZero_ReturnString");
+                }
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}
