2d71c359a85179b5bc9337addf2b545f26ca8920
Kovács Zétény Zsolt <Kovacs.Zeteny.Zsolt@AD.LOCAL>
2024-01-16 09:45:02 +0100

    Munkak

---
+++ Kovács Zétény Zsolt/UnitTestCourse/UnitTestCourse/WDF.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestCourse
+{
+    public class WDF
+    {
+        public string ReturnsPikachuIfZero(int num)
+        {
+            if(num == 0)
+            {
+                return "PIKACHU";
+            }
+            else
+            {
+                return "Sajnos nem Pikachu :(";
+            }
+        }
+    }
+}
