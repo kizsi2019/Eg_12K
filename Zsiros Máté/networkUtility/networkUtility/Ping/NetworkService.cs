eeb6ececa731367baa0a969fcfe8c29bcf6fc1cd
Zsiros Máté Ferenc <Zsiros.Mate.Ferenc@AD.LOCAL>
2024-01-16 10:39:34 +0100

    nem

--- Zsiros Máté/networkUtility/networkUtility/Ping/NetworkService.cs
+++ Zsiros Máté/networkUtility/networkUtility/Ping/NetworkService.cs
@@ -1,20 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace networkUtility.Ping
+namespace networkutility.Ping
 {
     public class NetworkService
     {
         public string SendPing()
         {
-            return "Success: Ping sent!";
+            return "Succes:Ping sent!";
         }
         public int PingTimeout(int a, int b)
         {
             return a + b;
+        }
+
+        public string PositiveOrNot(int a)
+        {
+            if(a < 0)
+            {
+                return ("Negative, it is.");
+            }
+            else if (a == 0)
+            {
+                return ("Zero, it is.");
+            }
+            else {
+                return ("Positive, it is.");
+            }
         }
     }
 }
