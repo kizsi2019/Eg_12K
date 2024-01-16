9539f899900576f6a94af7ffaf6ad8ef8955ba1c
Koncz Ákos <Koncz.Akos@AD.LOCAL>
2024-01-16 10:39:54 +0100

    ready

---
+++ Koncz Ákos/testing_2024_01_16/utilitytest/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace utilitytest
+{
+    public class Calculator
+    {
+        public int AddTwoNumbers(int first, int second)
+        {
+            return first + second;
+        }
+
+        public string IsAString(object input)
+        {
+            if (input == null || !(input is string))
+            {
+                return "It's not a text";
+            }
+
+            return (string)input;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calculator = new Calculator();
+        }
+    }
+}
