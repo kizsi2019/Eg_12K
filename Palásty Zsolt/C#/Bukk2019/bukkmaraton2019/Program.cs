a2a4036e4c1edb45327026552f02ec301b657ff3
Palásty Zsolt <palasty.zsolt19k@egressy.info>
2024-01-22 12:17:46 +0100

    ...

---
+++ Palásty Zsolt/C#/Bukk2019/bukkmaraton2019/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace bukkmaraton2019
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Versenyzo> versenyzos = new List<Versenyzo>();
+            foreach (var item in File.ReadAllLines("bukkm2019.txt").Skip(1))
+            {
+                versenyzos.Add(new Versenyzo(item));
+            }
+            Console.WriteLine($"versenytav nem teljesitettek: {(1-versenyzos.Count/691.0)*100}");
+
+            Console.ReadKey();
+        }
+    }
+}
