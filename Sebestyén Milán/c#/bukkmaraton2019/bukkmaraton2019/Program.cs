4579d325664ecbf657de4663dec3d77a72c4d8a1
Sebestyén Milán <sebestyen.milan19k@egressy.info>
2024-01-22 12:10:41 +0100

    bükkmaraton

--- Sebestyén Milán/c#/bukkmaraton2019/bukkmaraton2019/Program.cs
+++ Sebestyén Milán/c#/bukkmaraton2019/bukkmaraton2019/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<Versenyzo> versenyzos = new List<Versenyzo>();
+            foreach (var item in File.ReadAllLines("bukkm2019.txt").Skip(1))
+            {
+                versenyzos.Add(new Versenyzo(item));
+            }
+            Console.WriteLine($"versenytav nem teljesitettek: {(1-versenyzos.Count/691.0)*100}");
+
+            Console.ReadKey();
         }
     }
 }
