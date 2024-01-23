ea5fa7e1eac587e43377a0286bb6c0ada7daa656
Sebestyén Milán <sebestyen.milan19k@egressy.info>
2024-01-23 09:34:32 +0100

    bükmaraton

--- Sebestyén Milán/c#/bukkmaraton2019/bukkmaraton2019/Program.cs
+++ Sebestyén Milán/c#/bukkmaraton2019/bukkmaraton2019/Program.cs
@@ -11,6 +11,69 @@
             }
             Console.WriteLine($"versenytav nem teljesitettek: {(1-versenyzos.Count/691.0)*100}");
 
+            int ja = 0;
+            foreach (var item in versenyzos)
+            {
+                if (item.Noiversenyzo && Tav== "Rövid")
+                {
+                    ja++;
+                }
+
+            }
+            Console.WriteLine($"5. feladat: {ja}");
+
+            Console.WriteLine($"6. : {Tobbhatora.count ? "volt" : "nincs"}");
+            Versenyzo ferfirovid = null;
+            foreach (var item in versenyzos)
+            {
+                if (item.Tav=="Rövid" && item.Kategoria=="ff")
+                {
+                    if (ferfirovid==null)
+                    {
+
+                    }
+                    else
+                    {
+                        if (item.Ido < ferfirovid.Ido)
+                        {
+                            ferfirovid = item;
+                        }
+                    }
+                }
+                
+            }
+            Console.WriteLine(ferfirovid.Rajtszam);
+            Console.WriteLine(ferfirovid.Nev);
+            Console.WriteLine(ferfirovid.Ido);
+
+
+            Dictionary<string, int> dict = new Dictionary<string, int>();
+
+            foreach (var item in versenyzos)
+            {
+                int count1 = 0;
+                foreach (var item2 in versenyzos)
+                {
+                    if (item.Kategoria == item2.Kategoria && !item2.Noiversenyzo())
+                    {
+                        count1++;
+                    }
+                }
+                if (count1 > 0)
+                {
+                    dict[item.Kategoria] = count1;
+                }
+            }
+            Console.WriteLine("8. feladat: Statisztika");
+            foreach (KeyValuePair<string, int> item in dict)
+            {
+                Console.WriteLine($"\t{item.Key} - {item.Value} fő");
+            }
+
+
+
+
+
             Console.ReadKey();
         }
     }
