880c3b440e5755a7adb2f132d97678b24175ca5e
Városi Bendegúz Álmos <Varosi.Bendeguz@ad.local>
2024-01-23 12:59:54 +0100

    eret

---
+++ Városi Bendegúz/C#/nobeldijasok/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using ConsoleApp1;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //2
+            List<Dijazott> dijazottak = new List<Dijazott>();
+            foreach (var i in File.ReadAllLines("orvosi_nobeldijak.txt").Skip(1))
+            {
+                dijazottak.Add(new Dijazott(i));
+            }
+
+            //3
+            Console.WriteLine($"3.feladat: Díjazottak száma : {dijazottak.Count} fő");
+
+            //4
+            int utolsóÉv = dijazottak.First().Év;
+            foreach(var i in dijazottak.Skip(1))
+            {
+                if(i.Év > utolsóÉv)
+                {
+                    utolsóÉv = i.Év;
+                }
+            }
+            Console.WriteLine($"4. feladat: Utolsó év: {utolsóÉv}");
+
+            //5
+
+
+            Console.ReadKey();
+        }
+    }
+}
