bd416a7c3f8f27ef62cb9ca7f47d7dee17111a3f
Hézső Károly Ádám <Hezso.Karoly.Adam@ad.local>
2024-01-23 13:00:36 +0100

    ora

---
+++ Hézső Ádám Károly/Bukk2019_2/Bükmaraton2019_2/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Bükmaraton2019_2
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
