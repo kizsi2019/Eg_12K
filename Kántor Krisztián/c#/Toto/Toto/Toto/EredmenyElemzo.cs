49edd0beb07ad7d87d0deaa6339fcf1d027e1303
unknown <kantor.krisztian20@egressy.info>
2024-01-30 10:39:14 +0100

    asd

---
+++ Kántor Krisztián/c#/Toto/Toto/Toto/EredmenyElemzo.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Toto
+{
+    internal class EredmenyElemzo
+    {
+        private string Eredmenyek;
+
+        public string ev {  get; private set; }
+        public string het { get; private set; }
+        public int fordulo { get; private set; }
+        public int t13p1 { get; private set; }
+        public int ny13p1 { get; private set; }
+        public string eredmeny { get; private set; }
+
+        public EredmenyElemzo(string sor)
+        {
+            string[] s = sor.Split(';'); 
+            ev= s[0];
+            het = s[1];
+            fordulo = Convert.ToInt32(s[2]);
+            t13p1 = Convert.ToInt32(s[3]);
+            ny13p1 = Convert.ToInt32(s[4]);
+            eredmeny = s[5];
+        }
+
+        private int DontetlenekSzama
+        {
+            get
+            {
+                return Megszamol('X');
+            }
+        }
+
+        private int Megszamol(char kimenet)
+        {
+            int darab = 0;
+            foreach (var i in Eredmenyek)
+            {
+                if (i == kimenet) darab++;
+            }
+            return darab;
+        }
+
+        public bool NemvoltDontetlenMerkozes
+        {
+            get
+            {
+                return DontetlenekSzama == 0;
+            }
+        }
+
+        
+
+        
+    }
+}
---
+++ Kántor Krisztián/c#/Toto/Toto/Toto/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Toto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //2
+            List<EredmenyElemzo> lista = new List<EredmenyElemzo>();
+            foreach (var sor in File.ReadAllLines("toto.txt").Skip(1))
+            {
+                lista.Add(new EredmenyElemzo(sor));
+            }
+
+            //3
+            Console.WriteLine($"3. feladat: Fordulók száma: {lista.Count}");
+
+
+            //4
+            int count = 0;
+            foreach (EredmenyElemzo item in lista)
+            {
+                if (item.t13p1>0)
+                {
+                    count+=item.t13p1;
+                }
+            }
+            Console.WriteLine($"4. feladat: Telitalálatos szelvények száma: {count} db");
+
+            //5
+            double avg = 0;
+            Int64 nyeremenyOsszeg = 0;
+            int db = 0;
+            foreach(EredmenyElemzo item in lista)
+            {
+                if (item.t13p1>0)
+                {
+                    nyeremenyOsszeg += item.t13p1 * item.ny13p1;
+                    
+                    
+                }
+            }
+            avg = Math.Round( Convert.ToDouble(nyeremenyOsszeg)/ Convert.ToDouble(lista.Count));
+            Console.WriteLine($"5. feladat: Átlag: {avg} Ft");
+
+            //6
+            int maxi = -1;
+            int mini = -1;
+            for (int i = 1; i < lista.Count; i++)
+            {
+                if (lista[i].t13p1 > 0)
+                {
+                    if (maxi == -1) maxi = i;
+                    if (mini == -1) mini = i;
+                    if (lista[i].ny13p1 > lista[maxi].ny13p1)
+                    {
+                        maxi = i;
+                    }
+                    if (lista[i].ny13p1 < lista[mini].ny13p1)
+                    {
+                        mini = i;
+                    }
+                }
+            }
+            Console.WriteLine("\tLegnagyobb:");
+            Console.WriteLine($"\tÉv: {lista[maxi].ev}");
+            Console.WriteLine($"\tHét: {lista[maxi].het}.");
+            Console.WriteLine($"\tForduló: {lista[maxi].fordulo}.");
+            Console.WriteLine($"\tTelitalálat: {lista[maxi].t13p1} db");
+            Console.WriteLine($"\tNyeremény: {lista[maxi].ny13p1} Ft");
+            Console.WriteLine($"\tEredmények: {lista[maxi].eredmeny}");
+
+
+            Console.WriteLine("\n\tLegkisebb:");
+            Console.WriteLine($"\tÉv: {lista[mini].ev}");
+            Console.WriteLine($"\tHét: {lista[mini].het}.");
+            Console.WriteLine($"\tForduló: {lista[mini].fordulo}.");
+            Console.WriteLine($"\tTelitalálat: {lista[mini].t13p1} db");
+            Console.WriteLine($"\tNyeremény: {lista[mini].ny13p1} Ft");
+            Console.WriteLine($"\tEredmények: {lista[mini].eredmeny}");
+
+
+            //8
+            /*
+            bool dontetlen = false;
+            foreach (var i in lista)
+            {
+                EredmenyElemzo ell = new EredmenyElemzo(i.eredmeny);
+                if (ell.NemvoltDontetlenMerkozes)
+                {
+                    dontetlen = true;
+                    break;
+                }
+            }
+            Console.WriteLine($"8. feladat: {(dontetlen ? "Volt" : "Nem volt")} döntetlen nélküli forduló!");
+            */
+
+            Console.ReadKey();
+        }
+    }
+}
