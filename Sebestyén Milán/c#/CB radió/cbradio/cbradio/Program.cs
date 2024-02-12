5bb47073f5d82eb0c783955f35efcae7453a1780
Sebestyén Milán <sebestyen.milan19k@egressy.info>
2024-02-12 11:49:24 +0100

    cd

---
+++ Sebestyén Milán/c#/CB radió/cbradio/cbradio/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace cbradio
+{
+    internal class Program
+    {
+        struct Taxi
+        {
+            public int ora { get; set; }
+            public int perc { get; set; }
+            public int id { get; set; }
+            public string name { get; set; }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            List<Taxi> taxi = new List<Taxi>();
+            foreach (var item in File.ReadAllLines("bukkm2019.txt").Skip(1))
+            {
+                string[] sor = item.Split(';');
+                taxi.Add(new Taxi { ora = int.Parse(sor[0]),
+                    perc = int.Parse(sor[1]),
+
+                    });
+            }
+        }
+    }
+}
