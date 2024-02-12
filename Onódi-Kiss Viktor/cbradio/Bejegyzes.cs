9873488e7f291a8d38f1f4aa7d517b58d73d8208
Ónodi-Kiss Viktor <onodi.kiss.viktor19k@egressy.info>
2024-02-12 13:25:03 +0100

    0212

---
+++ Onódi-Kiss Viktor/cbradio/Bejegyzes.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cbradio
+{
+    public class Bejegyzes
+    {
+        public int Ora { get; set; }
+        public int Perc { get; set; }
+        public int AdasDb { get; set; }
+        public string Nev { get; set; }
+
+        public Bejegyzes(string line)
+        {
+            string[] temp = line.Split(';');
+            Ora = Convert.ToInt32(temp[0]);
+            Perc = Convert.ToInt32(temp[1]);
+            AdasDb = Convert.ToInt32(temp[2]);
+            Nev = temp[3];
+        }
+    }
+}
