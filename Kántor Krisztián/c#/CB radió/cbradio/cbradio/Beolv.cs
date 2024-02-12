292e9ee00f8045eee620882300e9ec1fb04c8832
unknown <kantor.krisztian20@egressy.info>
2024-02-12 12:01:42 +0100

    asd

---
+++ Kántor Krisztián/c#/CB radió/cbradio/cbradio/Beolv.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cbradio
+{
+    internal class Beolv
+    {
+        public int ora {  get; private set; }
+        public int perc { get; private set; }
+        public int db { get; private set; }
+        public string nev { get; private set; }
+
+        public Beolv(string sor)
+        {
+            string[] s=sor.Split(';');
+            ora = Convert.ToInt32( s[0]);
+            perc = Convert.ToInt32(s[1]);
+            db = Convert.ToInt32(s[2]);
+            nev = s[3];
+        }
+
+
+        //6
+        private int AtszamolPercre(int Ora, int Perc)
+        {
+            return Ora * 60 + Perc;
+        }
+
+
+        //7
+        public int Kezdes()
+        {
+            return AtszamolPercre(ora, perc);
+        }
+    }
+}
