0a1ca1daf1c379d22e3b67554d13b1405347cadd
unknown <kantor.krisztian20@egressy.info>
2024-02-13 13:21:11 +0100

    asd

--- Kántor Krisztián/tesztelés/NetworkUtility/NetworkUtility.Tests/PingTests/NetworkServiceTest.cs
+++ Kántor Krisztián/tesztelés/NetworkUtility/NetworkUtility.Tests/PingTests/NetworkServiceTest.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
+using FakeItEasy;
 using FluentAssertions;
 using FluentAssertions.Extensions;
+using NetworkUtility.DNS;
 using NetworkUtility.Ping;
 
 namespace NetworkUtility.Tests.PingTests
@@ -14,19 +17,23 @@ namespace NetworkUtility.Tests.PingTests
     {
 
         private readonly new NetworkService _pingService;
-
+        private readonly IDNS _dNS;
         public NetworkServiceTest()
-        { 
+        {
+            _dNS = A.Fake<IDNS>();
             //SUIT
-            _pingService=new NetworkService();
+            _pingService=new NetworkService(_dNS);
         }
         [Fact]
         public void NetworkService_SendPing_ReturnString()
         {
+            A.CallTo(() => _dNS.sendDNS()).Returns(true);
+
             //Act
             var result=_pingService.SendPing();
+            //Assert
             result.Should().NotBeNullOrWhiteSpace();
-            result.Should().Be("Sikeres Ping");
+            result.Should().Be("succesful Ping");
             result.Should().Contain("Ping", Exactly.Once());
         }
 
@@ -49,9 +56,10 @@ namespace NetworkUtility.Tests.PingTests
         public void NetworkService_PozitivNegativNulla_ReturnString(int a,string expected)
         {
             //Arrange
-            var pingService = new NetworkService();
+            var pingService = new NetworkService(_dNS);
             //Act
             string result = pingService.PozitivNegativNulla();
+            result.Should().BeOneOf("Pozitív", "Negatív", "Nulla");
             result.Should().BeOneOf("Pozitív", "Negatív", "Nulla");
         }
 
@@ -62,6 +70,37 @@ namespace NetworkUtility.Tests.PingTests
             var result = _pingService.LastPingDate();
             result.Should().BeAfter(1.January(2010));
             result.Should().BeBefore(1.January(2030));
+        }
+
+        [Fact]
+        public void NetworkService_GetPingOptions_ReturnObject()
+        {
+            var expected=new PingOptions()
+            {
+                DontFragment = true,
+                Ttl = 1
+            };
+            //Act
+            var result = _pingService.GetPingOptions();
+            result.Should().BeOfType<PingOptions>();
+            result.Should().BeEquivalentTo(expected);
+            result.Ttl.Should().Be(1);
+            result.DontFragment.Should().BeTrue();
+        }
+
+        [Fact]
+        public void NetworkService_MostRecentPings_ReturnObject()
+        {
+            var expected = new PingOptions()
+            {
+                DontFragment = true,
+                Ttl = 1
+            };
+            //Act
+            var result = _pingService.MostRecentPings();
+            result.Should().BeOfType<PingOptions[]>();
+            result.Should().ContainEquivalentOf(expected);
+            result.Should().Contain(x=> x.DontFragment==true);
         }
 
     }
