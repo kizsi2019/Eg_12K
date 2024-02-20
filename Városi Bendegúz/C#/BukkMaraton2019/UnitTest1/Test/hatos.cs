081546c5a3470855c8328a460a4934053155a8ac
Városi Bendegúz Álmos <Varosi.Bendeguz@AD.LOCAL>
2024-02-20 13:37:44 +0100

    eleg

---
+++ Városi Bendegúz/C#/BukkMaraton2019/UnitTest1/Test/hatos.cs
@@ -0,0 +1,47 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace UnitTest1.Test
+{
+    public class hatos
+    {
+        [Fact]
+        public void TobbMintHat_Should_ReturnTrue_When_VersenyzokContainTobbMintHat()
+        {
+            // Arrange
+            var versenyzok = new List<Versenyzo>
+            {
+                new Versenyzo { TobbMintHat = false },
+                new Versenyzo { TobbMintHat = true },
+                new Versenyzo { TobbMintHat = false }
+            };
+
+            // Act
+            bool tobbMintHat = false;
+            foreach (var v in versenyzok)
+            {
+                if (v.TobbMintHat)
+                {
+                    tobbMintHat = true;
+                    break;
+                }
+            }
+
+            // Assert
+            tobbMintHat.Should().BeTrue();
+        }
+
+
+    }
+
+    public class Versenyzo
+    {
+        public bool TobbMintHat { get; set; }
+    }
+}
+
---
+++ Városi Bendegúz/C#/BukkMaraton2019/UnitTest1/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace UnitTest1
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
