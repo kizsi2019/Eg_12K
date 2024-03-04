268fa21e19319105562a700a6810a9f753d8d766
Ónodi-Kiss Viktor <onodi.kiss.viktor19k@egressy.info>
2024-03-04 14:33:28 +0100

    0304

---
+++ Onódi-Kiss Viktor/CRUD/CRUT/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace CRUT
+{
+    public partial class Form1 : Form
+    {
+        static string server = "localhost";
+        static string uid = "root";
+        static string password = "";
+        static string database = "oscar";
+        static string conString = "server=" + server + ";uid=" + uid + ";pwd=" + password + ";database=" + database;
+        static MySqlConnection con = new MySqlConnection(conString);
+        public Form1()
+        {
+            InitializeComponent();
+            con.Open();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string insertTable = "insert into test_table(id, name, fname) values ("+tb1.Text+", '"+tb2.Text+"', '"+tb3.Text+"')";
+            MySqlCommand cmd = new MySqlCommand(insertTable, con);
+            int i = cmd.ExecuteNonQuery();
+            MessageBox.Show(i.ToString());
+        }
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            string createTable = "create table test_table(id int, name varchar(50), fname varchar(50))";
+            MySqlCommand cmd = new MySqlCommand(createTable, con);
+            int i = cmd.ExecuteNonQuery();
+            MessageBox.Show(i.ToString());
+        }
+
+        private void btnRead_Click(object sender, EventArgs e)
+        {
+            string sql = "select * from test_table";
+            MySqlCommand cmd = new MySqlCommand(sql, con);
+            MySqlDataReader reader = cmd.ExecuteReader();
+            DataTable dt = new DataTable();
+            dt.Load(reader);
+            dataGridView1.DataSource = dt;
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            string updateTable = "update test_table set name='Gyula' where id = 1";
+            MySqlCommand cmd = new MySqlCommand(updateTable, con);
+            int i = cmd.ExecuteNonQuery();
+            MessageBox.Show(i.ToString());
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            string delete = "Delete from test_table where id = 3";
+            MySqlCommand cmd = new MySqlCommand(delete, con);
+            int i = cmd.ExecuteNonQuery();
+            MessageBox.Show(i.ToString());
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            string sql = "select film.ev, film.cim from film where nyert order by ev;";
+            MySqlCommand cmd = new MySqlCommand(sql, con);
+            MySqlDataReader reader = cmd.ExecuteReader();
+            DataTable dt = new DataTable();
+            dt.Load(reader);
+            dataGridView1.DataSource = dt;
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            string sql = "";
+            MySqlCommand cmd = new MySqlCommand(sql, con);
+            MySqlDataReader reader = cmd.ExecuteReader();
+            DataTable dt = new DataTable();
+            dt.Load(reader);
+            dataGridView1.DataSource = dt;
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            string sql = "";
+            MySqlCommand cmd = new MySqlCommand(sql, con);
+            MySqlDataReader reader = cmd.ExecuteReader();
+            DataTable dt = new DataTable();
+            dt.Load(reader);
+            dataGridView1.DataSource = dt;
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+            string sql = "";
+            MySqlCommand cmd = new MySqlCommand(sql, con);
+            MySqlDataReader reader = cmd.ExecuteReader();
+            DataTable dt = new DataTable();
+            dt.Load(reader);
+            dataGridView1.DataSource = dt;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            string sql = "";
+            MySqlCommand cmd = new MySqlCommand(sql, con);
+            MySqlDataReader reader = cmd.ExecuteReader();
+            DataTable dt = new DataTable();
+            dt.Load(reader);
+            dataGridView1.DataSource = dt;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            string sql = "";
+            MySqlCommand cmd = new MySqlCommand(sql, con);
+            MySqlDataReader reader = cmd.ExecuteReader();
+            DataTable dt = new DataTable();
+            dt.Load(reader);
+            dataGridView1.DataSource = dt;
+        }
+    }
+}
