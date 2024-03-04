e5270d2b9ea4f3177c56df58230a34a9c23d0629
Koncz Ákos <Koncz.Akos@AD.LOCAL>
2024-03-04 14:01:50 +0100

    nig

--- Koncz Ákos/CRUT/CRUT/Form1.Designer.cs
+++ Koncz Ákos/CRUT/CRUT/Form1.Designer.cs
@@ -37,181 +37,191 @@
             this.tb2 = new System.Windows.Forms.TextBox();
             this.tb3 = new System.Windows.Forms.TextBox();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button6 = new System.Windows.Forms.Button();
-            this.button7 = new System.Windows.Forms.Button();
-            this.button8 = new System.Windows.Forms.Button();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // btnCreate
             // 
+            this.btnCreate.BackColor = System.Drawing.Color.Yellow;
             this.btnCreate.Location = new System.Drawing.Point(22, 10);
             this.btnCreate.Margin = new System.Windows.Forms.Padding(2);
             this.btnCreate.Name = "btnCreate";
             this.btnCreate.Size = new System.Drawing.Size(80, 36);
             this.btnCreate.TabIndex = 0;
             this.btnCreate.Text = "Create Table";
-            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.UseVisualStyleBackColor = false;
             this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
             // 
             // btnInsert
             // 
-            this.btnInsert.Location = new System.Drawing.Point(22, 50);
+            this.btnInsert.BackColor = System.Drawing.Color.Yellow;
+            this.btnInsert.Location = new System.Drawing.Point(22, 68);
             this.btnInsert.Margin = new System.Windows.Forms.Padding(2);
             this.btnInsert.Name = "btnInsert";
             this.btnInsert.Size = new System.Drawing.Size(80, 36);
             this.btnInsert.TabIndex = 1;
             this.btnInsert.Text = "Insert Data Tavle";
-            this.btnInsert.UseVisualStyleBackColor = true;
+            this.btnInsert.UseVisualStyleBackColor = false;
             this.btnInsert.Click += new System.EventHandler(this.button2_Click);
             // 
             // btnRead
             // 
-            this.btnRead.Location = new System.Drawing.Point(22, 91);
+            this.btnRead.BackColor = System.Drawing.Color.Yellow;
+            this.btnRead.Location = new System.Drawing.Point(22, 125);
             this.btnRead.Margin = new System.Windows.Forms.Padding(2);
             this.btnRead.Name = "btnRead";
             this.btnRead.Size = new System.Drawing.Size(80, 36);
             this.btnRead.TabIndex = 2;
             this.btnRead.Text = "Read Data Feom Table";
-            this.btnRead.UseVisualStyleBackColor = true;
+            this.btnRead.UseVisualStyleBackColor = false;
             this.btnRead.Click += new System.EventHandler(this.btnRead_Click);
             // 
             // btnUpdate
             // 
-            this.btnUpdate.Location = new System.Drawing.Point(22, 132);
+            this.btnUpdate.BackColor = System.Drawing.Color.Yellow;
+            this.btnUpdate.Location = new System.Drawing.Point(22, 187);
             this.btnUpdate.Margin = new System.Windows.Forms.Padding(2);
             this.btnUpdate.Name = "btnUpdate";
             this.btnUpdate.Size = new System.Drawing.Size(80, 36);
             this.btnUpdate.TabIndex = 3;
             this.btnUpdate.Text = "Update Table";
-            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.UseVisualStyleBackColor = false;
             this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(22, 172);
+            this.button5.BackColor = System.Drawing.Color.Yellow;
+            this.button5.Location = new System.Drawing.Point(22, 252);
             this.button5.Margin = new System.Windows.Forms.Padding(2);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(80, 36);
             this.button5.TabIndex = 4;
             this.button5.Text = "Delete Record";
-            this.button5.UseVisualStyleBackColor = true;
+            this.button5.UseVisualStyleBackColor = false;
             this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // tb1
             // 
-            this.tb1.Location = new System.Drawing.Point(196, 28);
+            this.tb1.Location = new System.Drawing.Point(152, 19);
             this.tb1.Margin = new System.Windows.Forms.Padding(2);
             this.tb1.Name = "tb1";
-            this.tb1.Size = new System.Drawing.Size(142, 20);
+            this.tb1.Size = new System.Drawing.Size(107, 20);
             this.tb1.TabIndex = 5;
             // 
             // tb2
             // 
-            this.tb2.Location = new System.Drawing.Point(196, 68);
+            this.tb2.Location = new System.Drawing.Point(152, 77);
             this.tb2.Margin = new System.Windows.Forms.Padding(2);
             this.tb2.Name = "tb2";
-            this.tb2.Size = new System.Drawing.Size(142, 20);
+            this.tb2.Size = new System.Drawing.Size(107, 20);
             this.tb2.TabIndex = 6;
             // 
             // tb3
             // 
-            this.tb3.Location = new System.Drawing.Point(196, 103);
+            this.tb3.Location = new System.Drawing.Point(152, 134);
             this.tb3.Margin = new System.Windows.Forms.Padding(2);
             this.tb3.Name = "tb3";
-            this.tb3.Size = new System.Drawing.Size(142, 20);
+            this.tb3.Size = new System.Drawing.Size(107, 20);
             this.tb3.TabIndex = 7;
             // 
             // dataGridView1
             // 
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.IndianRed;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(196, 127);
+            this.dataGridView1.Location = new System.Drawing.Point(297, 107);
             this.dataGridView1.Margin = new System.Windows.Forms.Padding(2);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
-            this.dataGridView1.Size = new System.Drawing.Size(382, 214);
+            this.dataGridView1.Size = new System.Drawing.Size(380, 311);
             this.dataGridView1.TabIndex = 10;
             // 
-            // button1
+            // btn1
             // 
-            this.button1.Location = new System.Drawing.Point(430, 10);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(35, 41);
-            this.button1.TabIndex = 11;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btn1.Location = new System.Drawing.Point(332, 23);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(26, 23);
+            this.btn1.TabIndex = 11;
+            this.btn1.Text = "1";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
             // 
-            // button2
+            // btn2
             // 
-            this.button2.Location = new System.Drawing.Point(358, 94);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 12;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            this.btn2.Location = new System.Drawing.Point(377, 23);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(26, 23);
+            this.btn2.TabIndex = 12;
+            this.btn2.Text = "2";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
             // 
-            // button3
+            // btn3
             // 
-            this.button3.Location = new System.Drawing.Point(390, 65);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 13;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
+            this.btn3.Location = new System.Drawing.Point(420, 23);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(26, 23);
+            this.btn3.TabIndex = 13;
+            this.btn3.Text = "3";
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.btn3_Click);
             // 
-            // button4
+            // btn4
             // 
-            this.button4.Location = new System.Drawing.Point(471, 63);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 23);
-            this.button4.TabIndex = 14;
-            this.button4.Text = "button4";
-            this.button4.UseVisualStyleBackColor = true;
+            this.btn4.Location = new System.Drawing.Point(469, 23);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(26, 23);
+            this.btn4.TabIndex = 14;
+            this.btn4.Text = "4";
+            this.btn4.UseVisualStyleBackColor = true;
+            this.btn4.Click += new System.EventHandler(this.button4_Click);
             // 
-            // button6
+            // btn5
             // 
-            this.button6.Location = new System.Drawing.Point(471, 28);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(75, 23);
-            this.button6.TabIndex = 15;
-            this.button6.Text = "button6";
-            this.button6.UseVisualStyleBackColor = true;
+            this.btn5.Location = new System.Drawing.Point(501, 23);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(26, 23);
+            this.btn5.TabIndex = 15;
+            this.btn5.Text = "5";
+            this.btn5.UseVisualStyleBackColor = true;
             // 
-            // button7
+            // btn6
             // 
-            this.button7.Location = new System.Drawing.Point(395, 10);
-            this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(29, 49);
-            this.button7.TabIndex = 16;
-            this.button7.Text = "button7";
-            this.button7.UseVisualStyleBackColor = true;
+            this.btn6.Location = new System.Drawing.Point(533, 23);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(26, 23);
+            this.btn6.TabIndex = 16;
+            this.btn6.Text = "6";
+            this.btn6.UseVisualStyleBackColor = true;
             // 
-            // button8
+            // btn7
             // 
-            this.button8.Location = new System.Drawing.Point(343, 10);
-            this.button8.Name = "button8";
-            this.button8.Size = new System.Drawing.Size(46, 39);
-            this.button8.TabIndex = 17;
-            this.button8.Text = "button8";
-            this.button8.UseVisualStyleBackColor = true;
+            this.btn7.Location = new System.Drawing.Point(581, 23);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(26, 23);
+            this.btn7.TabIndex = 17;
+            this.btn7.Text = "7";
+            this.btn7.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(600, 366);
-            this.Controls.Add(this.button8);
-            this.Controls.Add(this.button7);
-            this.Controls.Add(this.button6);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(755, 474);
+            this.Controls.Add(this.btn7);
+            this.Controls.Add(this.btn6);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.tb3);
             this.Controls.Add(this.tb2);
@@ -241,13 +251,13 @@
         private System.Windows.Forms.TextBox tb2;
         private System.Windows.Forms.TextBox tb3;
         private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button6;
-        private System.Windows.Forms.Button button7;
-        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn7;
     }
 }
 
--- Koncz Ákos/CRUT/CRUT/Form1.cs
+++ Koncz Ákos/CRUT/CRUT/Form1.cs
@@ -16,38 +16,19 @@ namespace CRUT
         string server = "localhost";
         string uid = "root";
         string password = "";
-        string database = "oscar";
+        string database = "oscar"; 
         public Form1()
         {
             InitializeComponent();
-
-            button1.Text = "1";
-            button2.Text = "2";
-            button3.Text = "3";
-            button4.Text = "4";
-            button6.Text = "6";
-            button7.Text = "7";
-            button8.Text = "8";
-
-            button1.BackColor = Color.Red;
-            button2.BackColor = Color.Blue;
-            button3.BackColor = Color.Bisque;
-            button4.BackColor = Color.BlanchedAlmond;
-            button6.BackColor = Color.DarkOliveGreen;
-            button7.BackColor = Color.Cyan;
-            button8.BackColor = Color.MediumPurple;
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
             string conString = "server=" + server + ";uid=" + uid + ";pwd=" + password + ";database=" + database;
-
             MySqlConnection con = new MySqlConnection(conString);
             con.Open();
-
-            string insertTable = "insert into text_table(id, name, fname) values (" + tb1.Text + ", '" + tb2.Text + "', '" + tb3.Text + "')";
+            string insertTable = "insert into test_table(id, name, fname) values ("+tb1.Text+", '"+tb2.Text+"', '"+tb3.Text+"')";
             MySqlCommand cmd = new MySqlCommand(insertTable, con);
-
             int i = cmd.ExecuteNonQuery();
             MessageBox.Show(i.ToString());
         }
@@ -55,13 +36,10 @@ namespace CRUT
         private void btnCreate_Click(object sender, EventArgs e)
         {
             string conString = "server=" + server + ";uid=" + uid + ";pwd=" + password + ";database=" + database;
-
             MySqlConnection con = new MySqlConnection(conString);
             con.Open();
-
-            string createTable = "create table text_table(id int, name varchar(50), fname varchar(50));";
+            string createTable = "create table test_table(id int, name varchar(50), fname varchar(50))";
             MySqlCommand cmd = new MySqlCommand(createTable, con);
-
             int i = cmd.ExecuteNonQuery();
             MessageBox.Show(i.ToString());
         }
@@ -69,31 +47,23 @@ namespace CRUT
         private void btnRead_Click(object sender, EventArgs e)
         {
             string conString = "server=" + server + ";uid=" + uid + ";pwd=" + password + ";database=" + database;
-
             MySqlConnection con = new MySqlConnection(conString);
             con.Open();
-
-            string sql = "SELECT * FROM text_table";
+            string sql = "select * from test_table";
             MySqlCommand cmd = new MySqlCommand(sql, con);
-
             MySqlDataReader reader = cmd.ExecuteReader();
             DataTable dt = new DataTable();
-
             dt.Load(reader);
-
             dataGridView1.DataSource = dt;
         }
 
         private void btnUpdate_Click(object sender, EventArgs e)
         {
             string conString = "server=" + server + ";uid=" + uid + ";pwd=" + password + ";database=" + database;
-
             MySqlConnection con = new MySqlConnection(conString);
             con.Open();
-
-            string updateTable = "UPDATE text_table SET name = 'Gyula' WHERE id = 1";
+            string updateTable = "update test_table set name='Gyula' where id = 1";
             MySqlCommand cmd = new MySqlCommand(updateTable, con);
-
             int i = cmd.ExecuteNonQuery();
             MessageBox.Show(i.ToString());
         }
@@ -101,32 +71,47 @@ namespace CRUT
         private void button5_Click(object sender, EventArgs e)
         {
             string conString = "server=" + server + ";uid=" + uid + ";pwd=" + password + ";database=" + database;
-
             MySqlConnection con = new MySqlConnection(conString);
             con.Open();
-
-            string deleteTable = "DELETE FROM text_table WHERE id = 1";
-            MySqlCommand cmd = new MySqlCommand(deleteTable, con);
-
+            string delete = "Delete from test_table where id = 3";
+            MySqlCommand cmd = new MySqlCommand(delete, con);
             int i = cmd.ExecuteNonQuery();
             MessageBox.Show(i.ToString());
         }
 
-        private void button2_Click_1(object sender, EventArgs e)
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
         {
             string conString = "server=" + server + ";uid=" + uid + ";pwd=" + password + ";database=" + database;
-
             MySqlConnection con = new MySqlConnection(conString);
             con.Open();
-
-            string sql = "SELECT * FROM oscar";
+            string sql = "select film.ev, film.cim from film where nyert order by ev;";
             MySqlCommand cmd = new MySqlCommand(sql, con);
-
             MySqlDataReader reader = cmd.ExecuteReader();
             DataTable dt = new DataTable();
-
             dt.Load(reader);
+            dataGridView1.DataSource = dt;
+        }
 
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            string conString = "server=" + server + ";uid=" + uid + ";pwd=" + password + ";database=" + database;
+            MySqlConnection con = new MySqlConnection(conString);
+            con.Open();
+            string sql = "";
+            MySqlCommand cmd = new MySqlCommand(sql, con);
+            MySqlDataReader reader = cmd.ExecuteReader();
+            DataTable dt = new DataTable();
+            dt.Load(reader);
             dataGridView1.DataSource = dt;
         }
     }
