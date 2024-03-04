4d922dc84de415009d463718c8996c121ad8c052
zanzitibi <kovacs.tito@gmail.com>
2024-03-04 14:09:59 +0100

    crud

--- Kovács Tibor Tamás/crud/crud/crud/Program.cs
+++ Kovács Tibor Tamás/crud/crud/crud/Program.cs
@@ -16,7 +16,7 @@ namespace crud
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Oscar());
         }
     }
 }
