6b4e4269a6a17f27d41f1e643193ca9fe7ae63e0
Ónodi-Kiss Viktor <onodi.kiss.viktor19k@egressy.info>
2024-04-08 13:30:13 +0200

    AIs projekt

---
+++ Onódi-Kiss Viktor/AI_Projekt/AI_Projekt/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Forms;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AI_Projekt
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            inputBox.PreviewMouseDown += (s, e) =>
+            {
+                var dialog = new FolderBrowserDialog();
+                dialog.Description = "Choose a folder";
+                var result = dialog.ShowDialog();
+                if (result.ToString() != "Cancel")
+                {
+                    inputBox.Text = dialog.SelectedPath;
+                }
+            };    
+        }
+    }
+}
