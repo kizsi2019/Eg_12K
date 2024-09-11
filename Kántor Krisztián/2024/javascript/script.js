20e391e63a8216e4e6de846b3ed64b9c5a711edc
Kántor Krisztián <kantor.krisztian@AD.LOCAL>
2024-09-11 10:34:36 +0200

    ad

--- Kántor Krisztián/2024/javascript/script.js
+++ Kántor Krisztián/2024/javascript/script.js
@@ -26,7 +26,8 @@ erzsi.korSzamitas()
 console.log(Szemely)
 console.log(odon)
 console.log(erzsi)
-*/
+
+//Objekt.create
 
 var SzemelyProto={
     korszamitas: function(){
@@ -44,3 +45,66 @@ var kati=create(SzemelyProto,{
     szuletesiEv:{value:1990},
     foglalkozas: {value:"kozmetikus"}
 })
+
+
+//egyszerű típusok és obj-ok
+
+var x=10
+var y=x
+x=100
+console.log(x)
+console.log(y)
+
+var o1={
+    sz: 100,
+    m:200
+}
+var o2=o1
+o1.sz=150
+
+console.log(o1.sz)
+console.log(o2.sz)
+
+var a =10
+var o= {
+    nev:"ödön",
+    kor:35
+}
+
+function modosit(a,b){
+    a=40
+    b.nev='Lajos'
+}
+
+modosit(a,o)
+
+console.log(a)
+console.log(o.nev)
+*/
+
+//fügvények
+
+var evek=[1954, 1990, 1963, 2000, 210]
+
+function tombMuvelet(tomb, fv){
+    var eredmeny=[]
+
+    for (var i = 0; i < tomb.length; i++) {
+        eredmeny.push(fv(tomb[i]))        
+    }
+    return eredmeny
+}
+
+
+function korSzamitas(elem){
+    return 2024-elem;
+}
+function felnott(elem){
+    return elem>=18;
+}
+
+var korok=tombMuvelet(evek,korSzamitas)
+console.log(korok)
+
+var felnottek= tombMuvelet(evek,felnott)
+console.log(felnottek)
